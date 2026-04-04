b3043ef04a8195e6dfc147eea98fc63126b280c7
mostafagamalbisher <mostafagamalbisher@outlook.com>
2026-04-04 08:46:24 +0300

    Refactor main.js to comment out unused hero section code for clarity

--- src/js/main.js
+++ src/js/main.js
@@ -9,14 +9,16 @@ const faqQuestions = document.querySelectorAll('.faq_question');
 
 faqQuestions.forEach((element) => clickLabel(element));
 
-const hero = document.querySelector('.hero-section');
+// const hero = document.querySelector('.hero-section');
 
-setTimeout(() => {
-  hero.classList.add('fade-in');
-}, 1000);
+// setTimeout(() => {
+//   hero.classList.add('fade-in');
+// }, 1000);
 
-const sections = document.querySelectorAll('.section');
+document.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll('.section');
 
-sections.forEach((section) => {
-  observer.observe(section);
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
 });
